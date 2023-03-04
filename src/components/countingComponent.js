885e5d55577eee22049e1bb0162c2560ce777eac
Iryna127 <irusia_1111@ukr.net>
2023-03-04 19:51:52 +0200

    changed

--- src/components/countingComponent.js
+++ src/components/countingComponent.js
@@ -1,18 +1,18 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const CountingState = () => {
   const [count, setCount] = useState(0);
   const onClickAddHendler = () => {
-   return setCount(count + 1);
-     };
-     const onClickSubHendler = () => {
-        return setCount(count - 1);
-          };
+    setCount(count + 1);
+  };
+  const onClickSubHendler = () => {
+    setCount(count - 1);
+  };
   return (
     <React.Fragment>
       <button onClick={onClickAddHendler}>+</button>
-      <button onClick ={onClickSubHendler}>-</button>
-      <p class='counter' >Counter: {count}</p>
+      <button onClick={onClickSubHendler}>-</button>
+      <p class="counter">Counter: {count}</p>
     </React.Fragment>
   );
 };
